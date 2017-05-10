7fc0f93a448c45a9f06ca387e6be240f794e7eed
Ezekiel <ezequiel.lichardi@ring.com>
2017-05-10 17:26:10 -0300

    Add companies & projects resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   root 'welcome#show'
 
-  resources :projects
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
-  resources :companies
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  namespace :admin, as: '' do
+    get 'dashboard', to: 'dashboard#show'
+    resources :projects
+    resources :companies
+  end
 end
